db197092ea5874080a43680c6748377c6864c936
Nirbhay Agrawal <agrawalnirbhay@yahoo.com>
2021-07-05 23:29:15 +0530

    Added DataLayer

---
+++ src/reducer.js
@@ -0,0 +1,27 @@
+export const initialState = {
+    user:null,
+    playlists:[],
+    playing:false,
+    item:null,
+    token:"BQAQhwNjMNBiNqSeauCXPtKCIs15eWlA9mrlb0Yp9xb41Zisy9jbhenCB7he40hYvmRkEycAjkQftx85InmN10_pHlnyOPePUu-TFRXwRAQNcs65SojYVi9tODN6lQciG5iEn7bRGe--vuo5sIqN7NQxi7Ej-fIzPVwWnClZ65IsmUMG"
+}
+
+const reducer=(state,action)=>{
+console.log(action);
+    switch(action.type)
+    {
+        case 'SET_USER':
+            return{
+                ...state,
+                user:action.user,
+            };
+        case 'SET_TOKEN':
+            return {
+                ...state,
+                token:action.token
+            }
+        default:
+            return state;
+    }
+}
+export default reducer;
